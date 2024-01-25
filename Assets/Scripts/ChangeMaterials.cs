b5bde8182513fab49cd605111e040bdb8060b5e4
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2024-01-25 16:35:42 -0600

    Clase 24 Ene

--- Assets/Scripts/ChangeMaterials.cs
+++ Assets/Scripts/ChangeMaterials.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class ChangeMaterials : MonoBehaviour
 {
 
-    Renderer rend;    
-    
+    public Renderer rend;
+
     public Material color1;
+    public Material color2; 
+    
+    [SerializeField]
+    Material color3;
+
+    [SerializeField]
+    Material color4; 
 
     // Start is called before the first frame update
     void Start()
@@ -25,14 +32,17 @@ public class ChangeMaterials : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.A)){
             //Debug.Log("Se presiono la tecla A"); //
+            rend.material = color2;
         }
 
         if(Input.GetKeyDown(KeyCode.S)){
             //Debug.Log("Se presiono la tecla S"); //
+            rend.material = color3;
         }
 
         if(Input.GetKeyDown(KeyCode.D)){
             //Debug.Log("Se presiono la tecla D"); //
+            rend.material = color4;
         }
     }
 }
